56300703ae6fd2d14a86ea189e1411f6431bfe7f
lizidong <12346>
2020-04-25 16:14:09 +0800

    更改

--- demo/dto/MySQLConfig.java
+++ demo/dto/MySQLConfig.java
@@ -2,13 +2,7 @@ package com.example.demo.dto;
 
 import org.hibernate.dialect.MySQL5Dialect;
 
-/**
- * 描述:
- * <p>
- * Created by mengxiansen on 2018-11-27 16:41
- *
- * @author mengxs@lenovocloud.com
- */
+
 public class MySQLConfig extends MySQL5Dialect {
     @Override
     public String getTableTypeString() {
